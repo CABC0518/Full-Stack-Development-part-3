3b32bd9cc50082fcf56801d0f3f336c73d52a753
SV-ta <takumi@vividi.fi>
2022-08-04 19:15:23 +0300

    3.5 done

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,7 +1,9 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -50,6 +52,38 @@ app.get('/api/persons/:id', (request, response) =>{
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    // get everrhthing except the deleted item
+    persons = persons.filter(p => p.id !== id)
+    console.log('delete called')
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log("body :",body)
+    let id = null
+    while(true){
+        id = Math.floor(Math.random() * 300000)
+        if(persons.find(p => p.id === id)){
+            console.log('continue')
+            continue
+        }else{
+            console.log('break')
+            break
+        }
+    }
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number,
+        date: new Date(),
+        
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

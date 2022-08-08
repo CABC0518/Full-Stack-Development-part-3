f8f0ef28468aded6d3e2eae45544534f8834d3ee
CABC0518 <takumi.aizawa1997@gmail.com>
2022-08-08 11:55:24 +0300

    3.18 done

--- phonebook/index.js
+++ phonebook/index.js
@@ -10,28 +10,6 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan('tiny'))
 
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 const gmtDateTime = new Date().toLocaleString()
 const generateID = (persons, id) =>{
     while(true){
@@ -46,41 +24,64 @@ const generateID = (persons, id) =>{
     }
 }
 
+
+
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>This is a welcome page</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-Person.find({}).then(person => {
+    console.log('this is called')
+    Person.find({}).then(person => {
         response.json(person)
     })
 })
 
 app.get('/info', (request, response) =>{
-    response.send(`
-    <P>Phonebook has info for ${persons.length} people</p>
-    <p>${gmtDateTime}</p>
-    `)
+    console.log('this is called')
+    Person.find({}).then(persons => {
+        response.send(`
+        <P>Phonebook has info for ${persons.length} people</p>
+        <p>${gmtDateTime}</p>
+        `)
+    })
+
 })
 
-app.get('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id) 
-    if(person){
-        response.json(person)
-    } else{
-        response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) =>{
+    Person.findById(request.params.id)
+    .then(person =>{
+        if(person){
+            response.json(person)
+        }else{
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next()) 
+})
+
+app.put('/api/persons/:id', (request, response, next) =>{
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
     }
-})
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    // get everrhthing except the deleted item
-    persons = persons.filter(p => p.id !== id)
-    console.log('delete called')
-    response.status(204).end()
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatePerson =>{
+        response.json(updatePerson)
+    })
+    .catch(error => next(error))
 })
-
 
 app.post('/api/persons', morgan(':method :url :status  :res[content-length] - :response-time ms :body'), (request, response) => {
     const body = request.body
@@ -90,21 +91,32 @@ app.post('/api/persons', morgan(':method :url :status  :res[content-length] - :r
             error: "Name or number is missing"
         })
     }
-    if(persons.find(p => p.name === body.name)){
-        return response.status(400).send({
-            error: "Name must be unique"
-        })
-    }
     const person = new Person({
         name: body.name,
         number: body.number,
         date: new Date(),
-        
     })
     person.save().then(savePerson => {
         response.json(savePerson)
     })
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'malformed id'})
+    }
+    next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
